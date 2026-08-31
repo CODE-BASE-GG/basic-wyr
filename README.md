90eb3b04b0973ffbd2bbfc1410fd2b4cb3e181a1
Banacount <uniquejmal@gmail.com>
2026-08-31 14:00:41 +0800

    A1: initial commit(design first, no backend contact)

---
+++ README.md
@@ -0,0 +1,4 @@
+# Just the design for now
+This is the front-end for the server-less
+would you rather api project. If you didn't notice,
+yes I will be using react.
---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="ui-contain">
+
+        <div className='rather-contain'>
+          <div className='left-side'>
+            Use neovim for every codebase forever.
+          </div>
+
+          <p className='middle-text'>OR</p>
+
+          <div className='right-side'>
+            Use vscode for every codebase forever.
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
